a3bf9a11b48ba9696da754ae09575ee1ce90f30f
rizki setyawan <rizki.delameta19@gmail.com>
2023-05-05 16:20:24 +0700

    initial threads.test

---
+++ src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,35 @@
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+const AuthenticationTokenManager = require('../../../Applications/security/AuthenticationTokenManager');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 401 if No Authentication', async () => {
+      // Arrange & Action
+      const server = await createServer(container);
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: {},
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(401);
+      expect(responseJson.error).toEqual('Unauthorized');
+      expect(responseJson.message).toEqual('Missing authentication');
+    });
+  });
+});
