7a55fe2e7ff0fe572b47cbf51aafbbb620b4883e
Rizki Setyawan <rizkisetyawan.dev@gmail.com>
2023-05-29 16:23:11 +0700

    pass say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -68,6 +68,6 @@ describe('when GET /', () => {
     // Assert
     const responseJson = JSON.parse(response.payload);
     expect(response.statusCode).toEqual(200);
-    expect(responseJson.value).toEqual('Hello world!');
+    expect(responseJson.value).toEqual('Hello world!!');
   });
 });
